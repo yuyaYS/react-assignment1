05d3ff8831477dd43bc3209d4f2945e3b74646d7
Yuya <y303sushi@gmail.com>
2021-01-09 09:07:02 +0900

    add delete functions

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
 import Table from './Table'
 
-const characters = [
+// const characters = [
+//     {
+//       name: 'Charlie',
+//       job: 'Janitor',
+//     },
+//     {
+//       name: 'Mac',
+//       job: 'Bouncer',
+//     },
+//     {
+//       name: 'Dee',
+//       job: 'Aspring actress',
+//     },
+//     {
+//       name: 'Dennis',
+//       job: 'Bartender',
+//     },
+// ];
+
+function MyApp() {
+  const [characters, setCharacters] = useState([  
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -18,14 +38,21 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
-
-function MyApp() {
+  ]);
     return (
         <div className="container">
-          <Table characterData={characters} />
+          <Table characterData={characters} removeCharacter ={removeOneCharacter}/>
         </div>
       )
+
+  function removeOneCharacter (index) {
+        const updated = characters.filter((character, i) => {
+           return i !== index
+        });
+        setCharacters(updated);
+     }
 }
+
+
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -17,6 +17,9 @@ function TableHeader() {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+              <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       );
     });
@@ -33,7 +36,8 @@ function TableHeader() {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} 
+                   removeCharacter={props.removeCharacter}/>
       </table>
     );
  }
